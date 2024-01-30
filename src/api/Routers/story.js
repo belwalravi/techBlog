8badd1b2a7c1f9551877624e021159a4901a7cb4
belwalravi <ravi.belwal@searce.com>
2024-01-30 21:01:32 +0530

    test

--- src/api/Routers/story.js
+++ src/api/Routers/story.js
@@ -4,6 +4,7 @@ const imageupload = require("../Helpers/Libraries/imageUpload");
 const {addStory,getAllStories,detailStory,likeStory, editStory, deleteStory, editStoryPage } = require("../Controllers/story")
 const { checkStoryExist, checkUserAndStoryExist } = require("../Middlewares/database/databaseErrorhandler");
 const { performAuth } = require("../Controllers/iapAuth");
+const { authenticator } = require("../Helpers/auth/authenticator");
 
 const router = express.Router() ;
 
@@ -22,7 +23,7 @@ router.get("/editStory/:slug",[performAuth,checkStoryExist,checkUserAndStoryExis
 router.put("/:slug/edit",[performAuth,checkStoryExist,checkUserAndStoryExist, imageupload.single("image")] ,editStory)
 
 // router.delete("/:slug/delete",[performAuth,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
-router.delete("/:slug/delete",[checkStoryExist,checkUserAndStoryExist] ,deleteStory)
+router.delete("/:slug/delete",[authenticator,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
 
 router.post("/:slug", detailStory)
 
