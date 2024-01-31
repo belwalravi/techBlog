f3e4cc159ede4c097c15dbe53efd9c96bbfd1eb5
belwalravi <ravi.belwal@searce.com>
2024-01-31 12:46:49 +0530

    test

--- src/www/components/StoryScreens/EditStory.js
+++ src/www/components/StoryScreens/EditStory.js
@@ -108,7 +108,9 @@ const EditStory = () => {
                                 <div class="absolute">
                                     Currently Image
                                 </div>
-                                <img src={`http://localhost:5000/storyImages/${previousImage}`} alt="storyImage" />
+                                {/* <img src={`http://localhost:5000/storyImages/${previousImage}`} alt="storyImage" /> */}
+                                <img src={`/storyImages/${previousImage}`} alt={title} />
+
                             </div>
                             <div class="StoryImageField">
                                 <AiOutlineUpload />
